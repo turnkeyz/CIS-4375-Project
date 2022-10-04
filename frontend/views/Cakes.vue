b2adead500687250a8a575392c6d608b67fd9936
gaguirr5 <gaguirr5@cougarnet.uh.edu>
2022-10-03 22:35:29 -0500

    product template

---
+++ frontend/views/Cakes.vue
@@ -0,0 +1,151 @@
+<template>
+<section style="background-color: #eee;">
+    <div class="container py-5">
+      <h4 class="text-center mb-5"><strong>Cakes</strong></h4>
+  
+      <div class="row">
+        <div class="col-lg-4 col-md-12 mb-4">
+          <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+            <img
+              src="../public/uploads/cakes/pexels-abhinav-goswami-291528.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+                <div class="d-flex justify-content-start align-items-start h-100">
+                  <h5><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">$123</span></h5>
+                </div>
+              </div>
+              <div class="hover-overlay">
+                <div
+                  class="mask"
+                  style="background-color: rgba(253, 253, 253, 0.15);"
+                ></div>
+              </div>
+            </a>
+          </div>
+        </div>
+  
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+            <img
+              src="../public/uploads/cakes/pexels-brent-keane-1702373.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+                <div class="d-flex justify-content-start align-items-start h-100">
+                  <h5><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">$239</span></h5>
+                </div>
+              </div>
+              <div class="hover-overlay">
+                <div
+                  class="mask"
+                  style="background-color: rgba(253, 253, 253, 0.15);"
+                ></div>
+              </div>
+            </a>
+          </div>
+        </div>
+  
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+            <img
+              src="../public/uploads/cakes/pexels-markus-spiske-369267.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+                <div class="d-flex justify-content-start align-items-start h-100">
+                  <h5><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">$147</span></h5>
+                </div>
+              </div>
+              <div class="hover-overlay">
+                <div
+                  class="mask"
+                  style="background-color: rgba(253, 253, 253, 0.15);"
+                ></div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+  
+      <div class="row">
+        <div class="col-lg-4 col-md-12 mb-4">
+          <div class="bg-image hover-zoom ripple shadow-1-strong rounded ripple-surface">
+            <img
+              src="../public/uploads/cakes/pexels-pixabay-461431.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+                <div class="d-flex justify-content-start align-items-start h-100">
+                  <h5><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">$83</span></h5>
+                </div>
+              </div>
+              <div class="hover-overlay">
+                <div
+                  class="mask"
+                  style="background-color: rgba(253, 253, 253, 0.15);"
+                ></div>
+              </div>
+            </a>
+          </div>
+        </div>
+  
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+            <img
+              src="../public/uploads/cakes/pexels-suzy-hazelwood-1098592.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+                <div class="d-flex justify-content-start align-items-start h-100">
+                  <h5><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">$106</span></h5>
+                </div>
+              </div>
+              <div class="hover-overlay">
+                <div
+                  class="mask"
+                  style="background-color: rgba(253, 253, 253, 0.15);"
+                ></div>
+              </div>
+            </a>
+          </div>
+        </div>
+  
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="bg-image hover-zoom ripple shadow-1-strong rounded">
+            <img
+              src="../public/uploads/cakes/pexels-vojtech-okenka-1055272.jpg"
+              class="w-100"
+            />
+            <a href="#!">
+              <div class="mask" style="background-color: rgba(0, 0, 0, 0.3);">
+                <div class="d-flex justify-content-start align-items-start h-100">
+                  <h5><span class="badge bg-light pt-2 ms-3 mt-3 text-dark">$58</span></h5>
+                </div>
+              </div>
+              <div class="hover-overlay">
+                <div
+                  class="mask"
+                  style="background-color: rgba(253, 253, 253, 0.15);"
+                ></div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  </template>
+
+  <style>
+
+    .w-100{
+        width: 200px;
+        height: 300px;
+    }
+  </style>
