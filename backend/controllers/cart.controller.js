6a8e36baf5b4c7b281b84578dafddbdbe24b0c94
Gabino Aguirre <gaguirr5@cougarnet.uh.edu>
2022-10-20 12:25:48 -0500

    add basic Cart CRUD backend

---
+++ backend/controllers/cart.controller.js
@@ -0,0 +1,32 @@
+const Cart = require("../model/cart.model");
+
+
+// add product
+exports.insertNewCart = async (req, res) => {
+  const obj = await Cart.insertCart(req.body);
+  res.send(obj);
+};
+
+// get all Cart
+exports.getAllCart = async (req, res) => {
+  const obj = await Cart.findAllFromCart();
+  res.send(obj);
+};
+
+// get one product
+exports.getCart = async (req, res) => {
+  const obj = await Cart.findCart(req.params.pid);
+  res.send(obj);
+};
+
+// update product
+exports.updateCart = async (req, res) => {
+  const obj = await Cart.updateCart(req.params.pid, req.body);
+  res.send(obj);
+};
+
+// delete product
+exports.deleteCart = async (req, res) => {
+  const obj = await Cart.deleteCartById(req.params.pid);
+  res.send(obj);
+};
