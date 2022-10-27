7039d7a10eb4d2e041441c06d95a5269f95e4a60
Gabino Aguirre <gaguirr5@cougarnet.uh.edu>
2022-10-27 09:35:55 -0500

    add navigation for worker portal, update orders backend for new order form

---
+++ frontend/src/methods/format_date.js
@@ -0,0 +1,13 @@
+function formatDateTimeFromSQLTOJS(datetime){
+    var arr1 = datetime.split('-')
+    var year = arr1[0]
+    var month = arr1[1]
+    var arr2 = arr1[2].split(':')
+    var day = arr2[0].slice(0,2)
+    var min = arr2[1]
+    var hour = arr2[0].slice(3,5)
+    let new_date = `${month}/${day}/${year}`
+    let new_time = `${hour}:${min}`
+    return [new_date, new_time]    
+}
+export {formatDateTimeFromSQLTOJS}
