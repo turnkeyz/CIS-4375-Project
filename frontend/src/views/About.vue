71c8c3bf1f4c5b4754a6904d50f8e2a40bea002e
gaguirr5 <gaguirr5@cougarnet.uh.edu>
2022-10-29 20:16:56 -0500

    margin cleanup

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -11,16 +11,16 @@
 <template>
   <main>
     <div class="container">
-        <h1 class="text-center mb-5"><strong>The Story</strong></h1>
+        <h1 class="text-center mb-2"><strong>The Story</strong></h1>
     </div>
 
-    <div class="container">
-        <p class="text-center mb-1">
+    <div class="container mb-5">
+        <p class="text-center mb-3">
             Owl B Baking was started in a kitchen of a small residential home here in the Houston area.
             After almost a decade of baking sweets for family, friends, and neighbors it was time to branch out
             and become a business open to the world where peple can find their next favorite dessert.  
         </p>
-        <p class="text-center mb-1">
+        <p class="text-center mt-3">
             After branching out across social media platforms it was time to find a home on the internet with our very own website!
         </p>
     </div>
