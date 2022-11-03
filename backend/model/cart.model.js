baf544642ddc62f407b61e76762ad42df0dec365
Gabino Aguirre <gaguirr5@cougarnet.uh.edu>
2022-11-03 01:03:29 -0500

    add routes to check if stuff exists in other tables

--- backend/model/cart.model.js
+++ backend/model/cart.model.js
@@ -157,4 +157,23 @@ Cart.getAllCartIds = () => {
 };
 
 
+
+Cart.fetchExistInOrder = (param_id) => {
+  return new Promise((resolve, reject) => {
+    sql.connect(sqlConfig, function (err, result) {
+      var request = new sql.Request()
+      .input("param_id", param_id)
+      request.query(
+      `SELECT COUNT(o.OrderID) as total
+      FROM Cart as c
+      LEFT JOIN Orders as o ON c.CartID=o.CartID
+      WHERE c.CartID=@param_id;`, (err, res) => {
+        if (err) reject(err);
+          console.log(res.recordset)
+          return resolve(res.recordset); // FETCHING ALL DATA
+      });
+    });
+  });
+};
+
 module.exports = Cart;
